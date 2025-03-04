8e4a8afb863c936d985f6f4bbbcde1610c396474
Lamspter1304 <josereyesmartinez1304@gmail.com>
2025-03-04 17:55:20 -0400

    Correción de estilos

--- user/auth/login.php
+++ user/auth/login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Eat&Meet - Login</title>
 
     <link rel="stylesheet" href="../../assets/css/normalize.css">
 
@@ -12,13 +12,13 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&family=SUSE:wght@100..800&display=swap" rel="stylesheet">
 
-    <link href="../../assets/css/login_style.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="../../assets/css/login_styles/login_style.css">
 </head>
 
 <body>
     <form class="formulario" action="#" method="POST">
-        <h2>Login</h2>
+        <h2>Login<span class = "p_span">¿Tienes Hambre?</span></h2>
+        
         <div class="seccion">
             <label for="email">Email</label>
             <input type="email" name="email" id="email" placeholder="Ingrese la email" required>
@@ -28,9 +28,10 @@
             <label for="password">Contraseña</label>
             <input type="password" name="password" id="password" placeholder="Ingrese la contraseña" required>
         </div>
-    <h3 class = "go_register">¿Aún no tienes cuenta? <a href="register.php">Regístrate</a></h3>
-
+        
         <button class="login" type="submit" class="btn-send">Login</button>
+       
+        <h3 class = "go_register">¿Aún no tienes cuenta? <a href="register.php">Regístrate</a></h3>
     </form>
 </body>
 
