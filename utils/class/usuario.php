cf913e8e00065c68c5fa726a65522c468ca4536f
Jose M. <20241548@itla.edu.do>
2025-03-04 18:32:45 -0400

    add: Implementar método en la clase usuario para validar credenciales.

--- utils/class/usuario.php
+++ utils/class/usuario.php
@@ -1,6 +1,41 @@
+//TODO: Agregar los metodos Setter and Getter
+
 <?php
 
 include_once 'conexionDB.php';
 
 
+
+include_once 'conexionDB.php';
+
+class usuario extends conectionDB
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function verificarUsuario($username, $password)
+    {
+        $query = "SELECT * FROM usuario WHERE nombre_usuario = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$username]);
+        $result = $stmt->fetch();
+
+
+        if (!$result) {  // * Si el usuario no existe, devolver falso
+            return false;
+        }
+
+
+        if (password_verify($password, $result['contraseña'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+}
 ?>
