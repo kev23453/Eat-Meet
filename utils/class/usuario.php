4c7de7ad3a9c74fb0141792511ee9abfd4408103
Dextron03 <brailyrs03@gmail.com>
2025-03-04 22:52:24 -0400

    Agregue la funcion verificarSesion, para verificar la sesion del usuario.

--- utils/class/usuario.php
+++ utils/class/usuario.php
@@ -7,8 +7,7 @@ include_once 'conexionDB.php';
 class usuario extends conectionDB
 {
     // metodo constructor heredando de la clase padre
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct();
     }
 
@@ -34,4 +33,14 @@ class usuario extends conectionDB
             echo $error->getMessage();
         }
     }
+
+    
+    public function verificarSesion($identificador, $path) {
+        // Este método verifica si existe una sesión activa mediante una cookie.
+        // Si la cookie con el identificador dado no está definida, redirige al usuario.
+        if (!isset($_COOKIE[$identificador])) {
+            header("Location: $path"); // Redirección a la página especificada
+            exit(); // Detiene la ejecución del script
+        }
+    }    
 }
