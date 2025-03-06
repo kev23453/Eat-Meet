43ea1a052a9caeda295851c7c2a4747b9c0f415e
GR3G0R12005 <gregorymartinez0212@gmail.com>
2025-03-05 23:25:26 -0400

    clase de notificaciones, que  recibe parámetros como el tipo de notificacion, el título, mensaje, estado, etc.

--- utils/class/notificacion.php
+++ utils/class/notificacion.php
@@ -1,7 +1,27 @@
 <?php
 
 class notificacion {
+
+    private $tipoNotificacion;
+    private $titulo;
+    private $mensaje;
+    private $estado;
    
+    public function __construct(){
+
+     
+    }
+
+    public function setNotificaciones($tipoNotificacion, $titulo,$mensaje, $estado){
+        $this->tipoNotificacion = $tipoNotificacion;
+        $this->titulo = $titulo;
+        $this->mensaje = $mensaje;
+        $this->estado = $estado;
+
+    }
+
+
+
 }
 
 ?>
